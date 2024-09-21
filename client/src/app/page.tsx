a346a16608f6183690e72e35fdc9e6ebeff160f2
Djeradé Golbé Parfait <70259653+Djerade@users.noreply.github.com>
2024-09-21 18:50:50 +0200

    update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,101 +1,85 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
+  const [fullName, setfullName] = useState("");
+  const [currentTechnologie, setcurrentTechnologie] = useState("");
+  const [Loading, setLoading] = useState(false);
+  const [image, setimage] = useState<File>();
+
+  function handleFormSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append("image", fullName);
+    formData.append("fullname", fullName);
+    formData.append("currentTechnologie", currentTechnologie);
+
+    console.log({
+      fullName,
+      image,
+      currentTechnologie,
+      Loading
+    });
+
+    axios
+      .post("", formData, {})
+      .then((res) => {
+        if (res.data.message) {
+          console.log(res.data.message);
+        }
+      })
+      .catch((err) => console.error(err.message));
+    setLoading(true);
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        <p className="font-bold size-1">Creat resume</p>
+        <form
+          className=" flex-col"
+          encType="multipart/form-data"
+          onSubmit={handleFormSubmit}
+          method="post"
+        >
+          <div className="flex-col w-full m-2">
+            <label htmlFor="fullName"> Enter your full Name</label>
+            <input
+              type="text"
+              name="fullName"
+              required
+              id="fullName"
+              value={fullName}
+              onChange={(e) => setfullName(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          </div>
+          <div className="flex-col w-full m-2">
+            <label htmlFor="currentTechnologie"> technologie</label>
+            <input
+              type="text"
+              name="currentTechnologie"
+              required
+              id="currentTechnologie"
+              value={currentTechnologie}
+              onChange={(e) => setcurrentTechnologie(e.target.value)}
+            />
+          </div>
+          <div className="flex-col w-full m-2">
+            <label htmlFor="photo"> Upload</label>
+            <input
+              type="file"
+              name="image"
+              required
+              id="image"
+              accept="image/x-png, image/jpeg"
+              onChange={(e) => e.target.files && setimage(e.target.files[0])}
+            />
+          </div>
+          <button type="submit">Create resume</button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const multer = require("multer");
+const { Configuration, OpenAIApi } = require("openai");
+const path = require("path");
 const cors = require("cors");
 const app = express();
 const PORT = 4000;
@@ -10,6 +13,31 @@ app.use(cors());
 app.get("/api", (req, res) => {
   res.json({
     message: "Hello world"
+  });
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }
+});
+
+app.use("/uploads", express.static("uploads"));
+
+app.post("/resume/create", upload.single("image"), async (req, res) => {
+  const { fullName, image, currentTechnologie } = req.body;
+  console.log(req.body, req.file);
+  res.json({
+    message: "Request successful!",
+    data: {}
   });
 });
 
